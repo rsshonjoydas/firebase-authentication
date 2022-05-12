8916d95986f03672b2467ccbbc2f3dd5e5999869
rsshonjoydas <rsshonjoydas@gmail.com>
2022-05-12 11:42:21 +0600

    profile complete

--- client/redux/services/uploadServices.ts
+++ client/redux/services/uploadServices.ts
@@ -1,36 +1,42 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-return-assign */
-/* eslint-disable no-return-await */
 /* eslint-disable import/prefer-default-export */
 import { getDownloadURL, listAll, ref, uploadBytesResumable } from 'firebase/storage';
 import { toast } from 'react-toastify';
 import { storage } from '../../firebase';
 
-const downloadFile = async (path: string) => {
-  let item: string = '';
-  await getDownloadURL(ref(storage, path))
-    .then((url) => (item = url))
-    .catch((err) => toast.error(err.message));
-
-  return item;
-};
-
 export const uploadFiles = async (folder: string, files: File[]) => {
-  const promise: any[] = [];
+  const promises: any[] = [];
 
   files.forEach((file) => {
     const storageRef = ref(storage, `${folder}/${file.name}`);
     const uploadTask = uploadBytesResumable(storageRef, file);
-    promise.push(uploadTask);
+    promises.push(uploadTask);
   });
 
-  const result = await Promise.all(promise);
-  const urlPromises = result.map(async (item) => {
+  const result: any[] = [];
+  await Promise.allSettled(promises).then((res) => {
+    res.forEach((item) => {
+      if (item.status === 'fulfilled') {
+        result.push(item.value);
+      }
+    });
+  });
+
+  const urlPromises = result.map((item) => {
     const path = item.ref.toString();
-    return await downloadFile(path);
+    return getDownloadURL(ref(storage, path));
   });
 
-  const urls = await Promise.all(urlPromises);
+  const urls: string[] = [];
+
+  await Promise.allSettled(urlPromises).then((res) => {
+    res.forEach((item) => {
+      if (item.status === 'fulfilled') {
+        urls.push(item.value);
+      }
+    });
+  });
+
   return urls;
 };
 
@@ -38,14 +44,23 @@ export const getFiles = async (folder: string, callback: (urls: string[]) => voi
   const listRef = ref(storage, `${folder}`);
 
   listAll(listRef)
-    .then(async (res) => {
-      const urlPromises = res.items.map(async (itemRef) => {
+    .then((res) => {
+      const urlPromises = res.items.map((itemRef) => {
         const path = itemRef.toString();
-        return await downloadFile(path);
+        return getDownloadURL(ref(storage, path));
       });
 
-      const url = await Promise.all(urlPromises).then((urls) => callback(urls));
-      return url;
+      return Promise.allSettled(urlPromises).then((response) => {
+        const urls: string[] = [];
+
+        response.forEach((item) => {
+          if (item.status === 'fulfilled') {
+            urls.push(item.value);
+          }
+        });
+
+        callback(urls);
+      });
     })
     .catch((err) => toast.error(err.message));
 };
